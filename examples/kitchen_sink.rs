6f8dbf5b1876de6f0814a441c1935feb5dd88f46
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-05-22 10:55:28 +0100

    fix up hat message handler macro todo: error handling in the macro

--- examples/kitchen_sink.rs
+++ examples/kitchen_sink.rs
@@ -5,31 +5,14 @@ use wasmtriggers_macros::init_function;
 use wasmtriggers_rs::{chat_message_handler, log::*};
 
 #[init_function]
-fn init_handler() {
+fn init() {
     debug("debug");
     info("info");
     warn("warn");
     error("error");
 }
-/*
-pub extern "C" fn chat_message_handler(ptr1: u32, len1: u32, ptr2: u32, len2: u32) {
-    unsafe {
-        let name =
-            str::from_utf8_unchecked(slice::from_raw_parts(ptr1 as *const u8, len1 as usize));
-        let message =
-            str::from_utf8_unchecked(slice::from_raw_parts(ptr2 as *const u8, len2 as usize));
-        __internal_chat_message_handler(name, message);
-    }
-}
-
-fn __internal_chat_message_handler(name: &str, msg: &str) {
-    unsafe {
-        info("chat message:");
-        info(name);
-        info(msg);
-    }
-}
-*/
 
 #[chat_message_handler]
-fn yamum() {}
+fn chat_message_logger(player: &str, message: &str) {
+    info(&format!("<{}>: {}", player, message).to_owned());
+}
--- wasmtriggers-macros/src/chat_message.rs
+++ wasmtriggers-macros/src/chat_message.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Attribute, ItemFn, parse_macro_input, parse_quote, spanned::Spanned};
+use syn::{ItemFn, parse_macro_input, parse_quote, spanned::Spanned};
 
 pub fn chat_message_handler(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as ItemFn);
@@ -14,21 +14,20 @@ pub fn chat_message_handler(_attr: TokenStream, item: TokenStream) -> TokenStrea
             .error("function shouldnt have a extern")
             .emit();
     }
-    let no_mangle: Attribute = parse_quote! {
-        #[unsafe(no_mangle)]
-    };
     let attrs = &input.attrs;
     let block = &input.block;
     let expanded = quote! {
     #[unsafe(no_mangle)]
     pub extern "C" fn chat_message_handler(name_ptr: u32, name_len: u32, msg_ptr: u32, msg_len:u32){
-        let name =
-            str::from_utf8_unchecked(slice::from_raw_parts(name_ptr as *const u8, name_len as usize));
-        let message =
-            str::from_utf8_unchecked(slice::from_raw_parts(msg_ptr as *const u8, msg_len as usize));
-        __internal_chat_message_handler(name, message);
+        unsafe {
+            let name =
+                str::from_utf8_unchecked(slice::from_raw_parts(name_ptr as *const u8, name_len as usize));
+            let message =
+                str::from_utf8_unchecked(slice::from_raw_parts(msg_ptr as *const u8, msg_len as usize));
+            __internal_chat_message_handler(name, message);
+        }
     }
-    #(#attrs)* #vis extern "C" #sig #block
+    #(#attrs)* #vis #sig #block
     };
     expanded.into()
 }
