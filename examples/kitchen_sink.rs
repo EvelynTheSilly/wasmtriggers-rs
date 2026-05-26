fa3d764fd7242b38c79cead7251cd59312340d29
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-05-26 12:33:48 +0100

    remove log spam from kitchen sink example

--- examples/kitchen_sink.rs
+++ examples/kitchen_sink.rs
@@ -27,25 +27,22 @@ fn funny_number_detector(chat: &ChatType) {
 }
 
 #[unsafe(no_mangle)]
+#[allow(unused)]
 pub extern "C" fn on_keyboard_input(action: u32, key_ptr: u32, key_len: u32) {
-    show_chat_message("some keyboard input just happened idk dont ask me");
+    // this happens on any keyboard input
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn on_keypress_a() {
-    show_chat_message("key a just pressed");
+    // this happens when a is pressed
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn on_keyrelease_a() {
-    show_chat_message("key a just released");
+    // this happens when a is released
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn on_keypress(ptr: u32, len: u32) {
-    let key;
-    unsafe { key = str::from_utf8(slice::from_raw_parts(ptr as *const u8, len as usize)) }
-    let _ = key.inspect(|key| {
-        show_chat_message(key);
-    });
+    // this happens on any key press event
 }
